5c8fee941b1e4c2cdcf2fe094714be0678d4b861
uma-kamuni <kamuniumaramesh@gmail.com>
2021-12-10 15:51:18 -0800

    Modified: Employee

--- employeewage.java
+++ employeewage.java
@@ -1,15 +1,18 @@
 public class empwage {
    public static void main(String[] args) {
    // Constants
-   int IS_FULL_TIME = 1;
+   int IS_PART_TIME = 1;
+   int IS_FULL_TIME = 2;
    int EMP_RATE_PER_HOUR = 20;
    // Variables
    int empHrs = 0;
    int empWage = 0;
    //Computation
-   double empCheck = Math.floor(Math.random() * 10) % 2;
+   double empCheck = Math.floor(Math.random() * 10) % 3;
    if (empCheck == IS_FULL_TIME)
        empHrs = 8;
+   else if (empCheck == IS_FULL_TIME)
+       empHrs = 4;
    empWage = empHrs * EMP_RATE_PER_HOUR;
    System.out.println("Emp Wage: " + empWage);
    }
