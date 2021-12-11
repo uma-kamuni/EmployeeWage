c7b783adbbd01b90fff335da06a28ae72bd3ec9c
uma-kamuni <kamuniumaramesh@gmail.com>
2021-12-10 17:48:02 -0800

    Modified: Monthly Employee Wage Is Calculated using While Loop

--- employeewage.java
+++ employeewage.java
@@ -4,25 +4,33 @@ public class empwage {
    public static final int IS_FULL_TIME = 2;
    public static final int EMP_RATE_PER_HOUR = 20;
    public static final int NUM_OF_WORKING_DAYS = 20;
-
+   public static final int MAX_HRS_IN_MONTH = 100;
 
 public static void main(String[] args) {
    // Variables
    int empHrs = 0;
    int empWage = 0;
+   int totalEmpHrs = 0;
    int totalEmpWage = 0;
+   int totalWorkingDays = 0;
    //Computation
-   for (int day = 0; day <  NUM_OF_WORKING_DAYS; day++) {
-   int empCheck = (int) Math.floor(Math.random() * 10) % 3;
-   switch (empCheck) {
-       case IS_FULL_TIME:
-           empHrs = 8;
-           break;
-       case IS_PART_TIME:
-           empHrs = 0;
-   }
+   while (totalEmpHrs <= MAX_HRS_IN_MONTH && totalWorkingDays < NUM_OF_WORKING_DAYS) {
+          empHrs = 0;
+          totalWorkingDays++;
+          int empCheck = (int) Math.floor(Math.random() * 10) % 3;
+         switch (empCheck) {
+             case IS_FULL_TIME:
+                empHrs = 8;
+                break;
+             case IS_PART_TIME:
+                empHrs = 4;
+                break;
+            default:
+              empHrs = 0;
+         }
+          totalEmpHrs += empHrs;
           empWage = empHrs * EMP_RATE_PER_HOUR;
-      	  totalEmpWage += empWage;
+          totalEmpWage += empWage;
           System.out.println("Emp Wage : " + empWage);
          }
           System.out.println("Total Emp Wage : " + totalEmpWage);
